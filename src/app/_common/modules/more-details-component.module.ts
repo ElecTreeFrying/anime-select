621e564bac5c257f27d34e71f284b189ec82baec
John James Ermitano <johnjamesermitano@yahoo.com>
2020-04-21 22:06:38 +0800

    chore: added media notify icons

--- src/app/_common/modules/more-details-component.module.ts
+++ src/app/_common/modules/more-details-component.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 import { SliceStringPipeModule } from './slice-string-pipe.module';
 import { AnimeDatePipeModule } from './anime-date-pipe.module';
@@ -13,6 +14,7 @@ import { MoreDetailsComponent } from '../../_components/more-details/more-detail
   ],
   imports: [
     CommonModule,
+    MatIconModule,
     AnimeDatePipeModule,
     SliceStringPipeModule,
   ],
